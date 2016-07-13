3b7cae4e31ae2e8cf933c2022fa59a264f12f5c8
我自己 <parkourcx@gmail.com>
2016-07-13 18:27:29 +0800

    add animations test

--- tableViewTest/ViewController.m
+++ tableViewTest/ViewController.m
@@ -32,8 +32,23 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@""];
     cell.textLabel.text = @"abc";
-    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d",indexPath.row];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%ld",(long)indexPath.row];
     return cell;
+}
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    UIView *v = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 50, 50)];
+    UILabel *l = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+    l.text = @"点击了";
+    v.backgroundColor = [UIColor blueColor];
+    [self.tableV addSubview:v];
+    [v addSubview:l];
+    [UIView animateWithDuration:1 animations:^{
+        v.alpha = 0;
+        
+    } completion:^(BOOL finished) {
+        NSLog(@"动画执行完毕");
+    }];
+    
 }
 
 
